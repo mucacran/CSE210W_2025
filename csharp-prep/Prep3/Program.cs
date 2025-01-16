4ca2b4a5c05c04c1e7ffbeafbe3dc3c6078270ea
mucacran <el.mucacran.rasta@hotmail.com>
2025-01-16 11:20:14 -0500

    actualizado

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string? yes = "yes";
+        while (yes != "no")
+        {
+            Random randomGenerator = new Random();
+            int numero = randomGenerator.Next(1, 101);
+
+            int nNumero = 0;
+            bool esNumeroValido = false;
+            bool again = true;
+
+            int track = 0;
+
+            while (!esNumeroValido || nNumero != numero)
+            {
+                Console.Write("What is your guess? ");
+                string? input = Console.ReadLine();
+
+                esNumeroValido = int.TryParse(input, out nNumero);
+
+                if (!esNumeroValido)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+                else if (nNumero > numero)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (nNumero < numero)
+                {
+                    Console.WriteLine("Higher");
+                }
+                track++;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine("track: " + track);
+
+
+
+            while(again){
+                Console.Write("Do you want to play again? ");
+                yes = Console.ReadLine()?.ToLower(); // Maneja la entrada nula y convierte a minúsculas.
+                if(yes == "no" || yes == "yes"){
+                    again = false;
+                }else{
+                    again = true;
+                }
+            }
+        }
     }
 }
