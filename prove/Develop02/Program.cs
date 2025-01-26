af9788bcc31146fa689f1786f5063f0d5fe87ef7
mucacran <el.mucacran.rasta@hotmail.com>
2025-01-25 20:33:24 -0500

    añadi una linea para saber donde esta mi uml
    en caso se me pierda

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,6 +2,8 @@
 // Create 6 menu items, where 3 is the one that deletes an entry.
 // You can create a file to save the entries and load them.
 
+////www.plantuml.com/plantuml/png/RL7BQiCm4BphAvQSRDByW2aXXOH0IQ6axQcKi5HB4EXX93cDAVttobe_Gh0FjdTcThIpseI8FZP6Ys-D8S2hTsUF1du5m19UKDaYHA_i-VCBq9z32HLSd9BYJwI7UvvTuotghcTr8fla52eug10VTwbgrqB0Sb0LPLSyIPcf8h_1qa_-6TsJPAj2hR7TiLznIxtXXURmWqCvXn_9EEve2sKZA2lfMbPJ3Z6bDtNSaoMFqVak49pg30Re8_4wrftldk8HhNI6-_APJBCji6i-c1VQcuqw1YDTxo69aHAqJPztPKXab1Em3DZf0VgoyoW5y5HRYgXqk1q8qaJ3vQxMR5N1uYFGOi8JCLpc37U_db6p8IiRe_y1
+
 using System;
 
 class Program
