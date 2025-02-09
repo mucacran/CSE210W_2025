641a40f0d00f59930b9f1e8380b443b8f94fd459
mucacran <el.mucacran.rasta@hotmail.com>
2025-02-08 20:09:43 -0500

    save this

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,8 +5,8 @@ class Fraction {
     private int _bottom;
 
     public Fraction(int numerator, int denominator) {
-        this._top = numerator;
-        this._bottom = denominator;
+        _top = numerator;
+        _bottom = denominator;
     }
 
     public Fraction(int _top) {
@@ -19,72 +19,26 @@ class Fraction {
         this._bottom = 1;
     }
 
-    public int get_top() {
+    private int getTop() {
         return this._top;
     }
 
-    public int getDenominator() {
+    private int getBottom() {
         return this._bottom;
     }
 
-    public void set_top(int _top) {
-        this._top = _top;
-    }
-
-    public void set_bottom(int _bottom) {
-        this._bottom = _bottom;
-    }
-
-    public void setFraction(int _top, int _bottom) {
-        this._top = _top;
-        this._bottom = _bottom;
-    }
-
-    public void setFraction(int _top) {
-        this._top = _top;
-        this._bottom = 1;
-    }
-
-    public void setFraction() {
-        this._top = 0;
-        this._bottom = 1;
-    }
-
-    public void add(Fraction f) {
-        this._top = this._top * f._bottom + f._top * this._bottom;
-        this._bottom = this._bottom * f._bottom;
-    }
-
-    public void substract(Fraction f) {
-        this._top = this._top * f._bottom - f._top * this._bottom;
-        this._bottom = this._bottom * f._bottom;
-    }
-
-    public void multiply(Fraction f) {
-        this._top = this._top * f._top;
-        this._bottom = this._bottom * f._bottom;
-    }
-
-    public void divide(Fraction f) {
-        this._top = this._top * f._bottom;
-        this._bottom = this._bottom * f._top;
-    }
-
-    public void simplify() {
-        int gcd = gcd(this._top, this._bottom);
-        this._top = this._top / gcd;
-        this._bottom = this._bottom / gcd;
-    }
-
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
+    private void setTop(int top) {
+        if(int.IsNegative(top)) {
+            throw new ArgumentException("Top cannot be negative");
         }
-        return gcd(b, a % b);
+        this._top = top;
     }
 
-    public String toString() {
-        return this._top + "/" + this._bottom;
+    private void setBottom(int bottom) {
+        this._bottom = bottom;
     }
-    
+
+    public void publicar(){
+        Console.WriteLine("Top: " + this.getTop());
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+
+
+        var Fraction1 = new Fraction();
+        var Fraction2 = new Fraction(6);
+        var Fraction3 = new Fraction(6,7);
+
+        Fraction2.publicar();
+        
     }
+
+
 }
