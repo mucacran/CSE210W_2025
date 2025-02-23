5ca7393226f6524abeee2383c329226b323fce88
mucacran <el.mucacran.rasta@hotmail.com>
2025-02-22 20:21:35 -0500

    avances
    son mis avances de esta semana

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool running = true;
+        
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflection Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Start Gratitude Activity");
+            Console.WriteLine("  5. Quit");
+            Console.Write("Select a choise from the menu: ");
+            
+            string choice = Console.ReadLine();
+            
+            Activity activity = null;
+            
+            switch (choice)
+            {
+                case "1":
+                    activity = new BreathingActivity();
+                    break;
+                case "2":
+                    activity = new ReflectionActivity();
+                    break;
+                case "3":
+                    activity = new ListingActivity();
+                    break;
+                case "4":
+                    activity = new GratitudeActivity();
+                    break;
+                case "5":
+                    running = false;
+                    continue;
+                default:
+                    Console.WriteLine("Invalid choice. Press enter to continue...");
+                    Console.ReadLine();
+                    continue;
+            }
+            
+            if (activity != null)
+            {
+                activity.RunActivity();
+            }
+        }
     }
 }
