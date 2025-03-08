fe0d25f805f7e7e86d31f4d398d591b1b83406de
mucacran <el.mucacran.rasta@hotmail.com>
2025-03-08 12:35:57 -0500

    termine

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -18,18 +18,6 @@ public abstract class Shape
     {
         _color = color;
     }
-
-    // Because it does not make sense to define a default body for this method in the
-    // base class, rather than make a "virtual" function here like this:
-    //
-    // public virtual double GetArea()
-    // {
-    //     return 0;
-    // }
-    //
-    // We can instead declare the function as an "abstract" function. That means
-    // that it is an empty virtual function that must be implemented or "filled in" by
-    // any class that inherits from Shape. Then, any class that has an abstract method
-    // must also be declared to be abstract.
-    public abstract double GetArea();
+    
+    public virtual double GetArea(){return 0;}
 }
